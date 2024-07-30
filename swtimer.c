a44b11f74f5ba127234e3fbfb4a5c85d393e36d2
lattecoff <70164468+lattecoff@users.noreply.github.com>
2024-07-30 14:49:13 +0300

    Fix timer.

--- swtimer.c
+++ swtimer.c
@@ -1,49 +1,187 @@
+/**
+ *@file sw_timer.c
+ *@brief Управление программными таймерами.
+ *@author lattecoff.
+ */
 
+#include "stddef.h"
+#include "stdbool.h"
+#include "sw_timer.h"
+ 
+#define SW_TIMERS_AMOUNTS	40
 
-#include "string.h"
+swtmr_t *pSwTimers[SW_TIMERS_AMOUNTS] = {NULL};
 
-#include "swtimer.h"
-
-
-#define SWTMR_AMOUNT	5
-
-
-swtimer_t SwTimers[SWTMR_AMOUNT];
-
-
-void SwTimer_Init(void)
+/**
+ * @param[in] *pTIMER - указатель на требуемый программный таймер.
+ * @param[in] MODE - режим.
+ * @param[in] SET_TIME - устанавливаемое время, через которое сработает таймер.
+ * @note SET_TIME = 0 устанавливает таймер в максимальное значение.
+ */	
+void SwTimer_Create(swtmr_t *const pTIMER, const swtmr_md_t MODE, const uint32_t SET_TIME)
 {
-	memset(SwTimers, 0xFF, sizeof(SwTimers));
-}
-
-void SwTimer_Create(swtimer_t **pDevTimer, const swtimer_st_t state, const uint32_t SetTime)
-{
-	uint8_t id = 0;
-
-
-
-	while (id < SWTMR_AMOUNT)
+	volatile uint8_t id = 0;
+	volatile bool isErr = false;
+	
+	
+	while ((pSwTimers[id] != NULL) && (pSwTimers[id] != pTIMER))
 	{
-		id++;
-		if (id >= SWTMR_AMOUNT)
+		if(id < SW_TIMERS_AMOUNTS)
 		{
-			// overflow sw_timers.
-			break;
+			id++;
 		}
-		else if (SwTimers[id].state == 0xFF)
+		else
 		{
-			// create success.
-			if (SetTime)
-				SwTimers[id].endTime = SetTime;
-			else
-				SwTimers[id].endTime = 0xFFFFFFFF -1;
-
-
-			SwTimers[id].currTime = 0;
-			SwTimers[id].state = state;
-
-			*pDevTimer = (swtimer_t *)&SwTimers[id];
+			isErr = true;
 			break;
 		}
 	}
+	
+	if (!isErr)
+	{
+		pSwTimers[id] = pTIMER;
+		pTIMER->endTime = SET_TIME != 0UL ? SET_TIME : (0xFFFFFFFFUL -1);
+		pTIMER->currTime = 0;
+		pTIMER->isTimer = MODE;
+	}
+	else
+	{
+		/* do not created swtimer. */
+	}
 }
+
+/**
+ * @param[in] *pTimer - указатель на требуемый программный таймер.
+ */
+void SwTimer_Delete(swtmr_t *pTimer)
+{
+	volatile uint8_t id = 0;
+	volatile bool isErr = false;
+	
+	
+	while (pSwTimers[id] != pTimer)
+	{
+		if(id < SW_TIMERS_AMOUNTS)
+		{
+			id++;
+		}
+		else
+		{
+			isErr = true;
+			break;
+		}
+	}
+
+	if (!isErr)
+	{
+		pSwTimers[id]->isTimer = SWTMR_IDLE;
+		pSwTimers[id]->endTime = 0;
+		pSwTimers[id]->currTime = 0;
+		pSwTimers[id] = NULL;
+	}
+	else
+	{
+		/* do not deleted swtimer. */
+	}
+}
+
+/**
+ * @param[in] *pTIMER - указатель на требуемый программный таймер.
+ * @param[in] MODE - режим.
+ * @param[in] SET_TIME - устанавливаемое время, через которое сработает таймер.
+ * @note SET_TIME = 0 устанавливает таймер в максимальное значение.
+ */
+void SwTimer_Set(swtmr_t *const pTIMER, const swtmr_md_t MODE, const uint32_t SET_TIME)
+{
+	volatile uint8_t id = 0;
+	volatile bool isErr = false;
+	
+	while (pSwTimers[id] != pTIMER)
+	{
+		if(id < SW_TIMERS_AMOUNTS)
+		{
+			id++;
+		}
+		else
+		{
+			isErr = true;
+			break;
+		}
+	}
+	
+	if (!isErr)
+	{
+		pTIMER->endTime = SET_TIME != 0UL ? SET_TIME : (0xFFFFFFFFUL -1);
+		pTIMER->currTime = 0;
+		pTIMER->isTimer = MODE;
+	}
+	else
+	{
+		/* do not set swtimer. */
+	}
+}
+
+/**
+ * @param[in] *pTIMER - указатель на требуемый программный таймер.
+ * @retval SWTMR_IDLE.
+ * @retval SWTMR_RUN.
+ * @retval SWTMR_DONE.
+ */
+swtmr_md_t SwTimer_Get_Status(swtmr_t * const pTIMER)
+{
+	return pTIMER->isTimer;
+}
+
+/**
+ * @param[in] *pTIMER - указатель на требуемый программный таймер.
+ * @return Текущее значение таймера.
+ */
+uint32_t SwTimer_Get_Time(swtmr_t * const pTIMER)
+{
+	return pTIMER->currTime;
+}
+
+/**
+ * @param[in] *pTIMER - указатель на требуемый программный таймер.
+ * @return Остаток времени.
+ */
+uint32_t SwTimer_Get_Rest_Time(swtmr_t *const pTIMER)
+{
+	return pTIMER->endTime - pTIMER->currTime;
+}
+
+/**
+
+ */
+inline void SwTimer_Scan(void)
+{
+	static uint8_t numCalls = 0;
+	volatile uint8_t id = 0;
+
+
+	if ((++numCalls) >= 100)
+	{
+		numCalls = 0;
+
+		while (id < SW_TIMERS_AMOUNTS)
+		{
+			if (pSwTimers[id] != NULL)
+			{
+				if (pSwTimers[id]->isTimer == SWTMR_RUN)
+				{
+					#if (REF_CONF_RELEASE)
+					(pSwTimers[id]->currTime)++;
+					
+					if ((pSwTimers[id]->currTime) >= (pSwTimers[id]->endTime))
+					{
+						pSwTimers[id]->isTimer = SWTMR_DONE;
+					}
+				}
+			}
+
+			id++;
+		}
+	}
+}
+
+/* end of file. */
--- swtimer.h
+++ swtimer.h
@@ -1,25 +1,50 @@
+/**
+ *@file sw_timer.h
+ *@brief
+ *@author lattecoff.
+ */
 
+#include "r_cg_macrodriver.h"
 
-#ifndef SWTIMER_H_
-#define SWTIMER_H_
+#ifndef SW_TIMER_H
+#define SW_TIMER_H
+
+#ifdef REF_CONF_RELEASE
+#define _SEC	*1U
+#define _MIN	*60U
+#define _HOUR	*60UL *60UL
+
+#else
+#define _SEC	*1U
+#define _MIN	*60U /** *10U */
+#define _HOUR	*60UL *60UL /** *10UL *10UL */
+
+#endif
+
+#define SWTMR_MAX_TIME	(0xFFFFFFFEUL)	// 0xFFFFFFFF -1.
+#define SWTMR_MIN_TIME	(0U)
 
 typedef enum {
-	SWTMR_IDLE,
-	SWTMR_RUN,
-	SWTMR_DONE,
-} swtimer_st_t;
+	SWTMR_IDLE,	///< таймер остановлен.
+	SWTMR_RUN,	///< таймер запущен.
+	SWTMR_DONE	///< таймер выполнен.
+} swtmr_md_t;
 
-
-typedef struct 
-{
+typedef struct swtmr_t {
 	uint32_t endTime;
 	uint32_t currTime;
-	swtimer_st_t state;
-} swtimer_t;
+	swtmr_md_t isTimer;
+} swtmr_t;
 
-void SwTimer_Init(void);
-void SwTimer_Create(swtimer_t **pDevTimer, const swtimer_st_t state, const uint32_t SetTime);
+void SwTimer_Create(swtmr_t *const pTIMER, const swtmr_md_t MODE, const uint32_t SET_TIME);
 
-extern swtimer_t SwTimers[];
+void SwTimer_Delete(swtmr_t *pTimer);
+void SwTimer_Set(swtmr_t *const pTIMER, const swtmr_md_t MODE, const uint32_t SET_TIME);
+swtmr_md_t SwTimer_Get_Status(swtmr_t *const pTIMER);
+uint32_t SwTimer_Get_Rest_Time(swtmr_t *const pTIMER);
+uint32_t SwTimer_Get_Time(swtmr_t *const pTIMER);
+void SwTimer_Scan(void);
 
-#endif /* SWTIMER_H_ */
+#endif /* SW_TIMER_H */ 
+
+/* end of file. */
